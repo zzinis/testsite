1e7f116adaf15792b8834f18055f1f9c0573494b
zzinis <lizzyin@naver.com>
2023-10-28 21:14:39 +0900

    app route 설정

--- site/src/App.js
+++ site/src/App.js
@@ -1,9 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route } from 'react-router-dom';
+
+//common
+import Footer from './components/common/Footer';
+import Header from './components/common/Header';
+
+//main
+import Visual from './components/main/Visual';
+
+//sub
+import Community from './components/sub/Community';
+import Contact from './components/sub/Contact';
+import Department from './components/sub/Department';
+import Gallery from './components/sub/Gallery';
+import Member from './components/sub/Member';
+import Youtube from './components/sub/Youtube';
+
 
 function App() {
   return (
     <>
+      <Header />
+      <Route path='/'>
+        <Visual />
+      </Route>
+
+      <Route path='/department'>
+        <Department />
+      </Route>
+
+      <Route path='/community'>
+        <Community />
+      </Route>
+
+      <Route path='/gallery'>
+        <Gallery />
+      </Route>
+
+      <Route path='/youtube'>
+        <Youtube />
+      </Route>
+
+      <Route path='/contact'>
+        <Contact />
+      </Route>
+
+      <Route path='/member'>
+        <Member />
+      </Route>
+
+      <Footer />
 
     </>
   );
