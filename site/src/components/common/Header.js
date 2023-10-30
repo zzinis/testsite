1abf9e585e421dbfaf75b5e36c45d94ebac489c6
zzinis <lizzyin@naver.com>
2023-10-30 18:46:38 +0900

    기본 라우터

--- site/src/components/common/Header.js
+++ site/src/components/common/Header.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom';
 
 function Header() {
     return (
-        <div>Header</div>
-    )
+        <header>
+            <h1>
+                <Link to='/'>LOGO</Link>
+            </h1>
+
+            <ul id='gnb'>
+                <li>
+                    <NavLink to='/department'>Department</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/community'>Community</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/gallery'>Gallery</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/youtube'>Youtube</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/contact'>Contact</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/member'>Member</NavLink>
+                </li>
+            </ul>
+        </header>
+    );
 }
 
-export default Header
+export default Header;
