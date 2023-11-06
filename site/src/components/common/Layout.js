4b54d1d31784f2e3e0ad2b42391644209c3bdc00
zzinis <lizzyin@naver.com>
2023-11-06 22:49:17 +0900

    화면 전환 모션 추가

--- site/src/components/common/Layout.js
+++ site/src/components/common/Layout.js
@@ -1,6 +1,12 @@
+import { useEffect, useRef } from 'react';
+
 function Layout({ name, children }) {
+    const frame = useRef(null);
+    useEffect(() => {
+        frame.current.classList.add('on');
+    }, []);
     return (
-        <section className={`content ${name}`}>
+        <section className={`content ${name}`} ref={frame}>
             <figure></figure>
             <div className='inner'>
                 <h1>{name}</h1>
--- site/src/components/sub/Department.js
+++ site/src/components/sub/Department.js
@@ -8,6 +8,6 @@ function Department() {
         </Layout>
     );
 }
-}
+
 
 export default Department
