53af1370bb7487dfabba2fe9d88056329ce345b1
zzinis <lizzyin@naver.com>
2023-11-08 20:54:45 +0900

    조직도 외부 데이터 fetching
    컴포넌트 마운트시 외부 데이터 fetching후 state에 저장한뒤, 반복 렌더링 처리

--- site/src/components/sub/Department.js
+++ site/src/components/sub/Department.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 import Layout from '../common/Layout';
 
 function Department() {
+    const [Members, setMembers] = useState([]);
+    console.log(Members);
+    useEffect(() => {
+        axios.get(`${process.env.PUBLIC_URL}/DB/members.json`).then((data) => {
+            setMembers(data.data.members);
+        });
+    }, []);
+
     return (
         <Layout name={'Department'}>
-            <p>회사 조직소개 페이지입니다.</p>
+            {Members.map((member, idx) => {
+                return (
+                    <article key={idx}>
+                        <div className='pic'>
+                            <img src={`${process.env.PUBLIC_URL}/img/${member.pic}`} alt={member.name} />
+                        </div>
+                        <h2>{member.name}</h2>
+                        <p>{member.position}</p>
+                    </article>
+                );
+            })}
         </Layout>
     );
 }
