14d90d6b7c34678768e43c4e5bd9b434b230b6d4
zzinis <lizzyin@naver.com>
2023-11-10 20:41:39 +0900

    유튜브 데이터 fetching

--- site/src/components/sub/Youtube.js
+++ site/src/components/sub/Youtube.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Layout from '../common/Layout';
 
 function Youtube() {
-    return <Layout name={'Youtube'}></Layout>;
+    const [Vids, setVids] = useState([]);
+    useEffect(() => {
+        const key = 'key값';
+        const list = 'list값';
+        const num = 10;
+        const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${list}&key=${key}&maxResults=${num}`;
+        axios.get(url).then((data) => {
+            console.log(data.data.items);
+            setVids(data.data.items);
+        });
+    }, []);
+    return (
+        <Layout name={'Youtube'}>
+            {Vids.map((vid, idx) => {
+                return (
+                    <article key={idx}>
+                        <img src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
+                        <h2>{vid.snippet.title}</h2>
+                        <p>{vid.snippet.description}</p>
+                        <span>{vid.snippet.publishedAt}</span>
+                    </article>
+                );
+            })}
+        </Layout>
+    );
 }
 
 export default Youtube
