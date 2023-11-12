a54ef3ceac63076252f9c8b8e35c8cdcf987026b
zzinis <lizzyin@naver.com>
2023-11-12 22:53:34 +0900

    모달 컴포넌트 생성

---
+++ site/src/components/common/Modal.js
@@ -0,0 +1,9 @@
+const Modal = () => {
+    return (
+        <aside className='modal'>
+            <div className='con'></div>
+            <span className='close'>close</span>
+        </aside>
+    );
+};
+export default Modal;
