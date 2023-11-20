32a8ca957d523d38255aefb078dc4d82526d34ba
zzinis <lizzyin@naver.com>
2023-11-20 19:27:23 +0900

    버튼 클릭시 세로 스크롤 이동 모션 구현

--- site/src/components/main/Btns.js
+++ site/src/components/main/Btns.js
@@ -1,4 +1,5 @@
 import { useRef, useEffect, useState } from 'react';
+import Anime from '../../asset/anime';
 
 function Btns() {
     const btnRef = useRef(null);
@@ -24,7 +25,18 @@ function Btns() {
             {Array(Num)
                 .fill()
                 .map((_, idx) => {
-                    return <li key={idx}></li>;
+                    return (
+                        <li
+                            key={idx}
+                            onClick={() => {
+                                new Anime(window, {
+                                    prop: 'scroll',
+                                    value: pos.current[idx],
+                                    duration: 500,
+                                });
+                            }}
+                        ></li>
+                    );
                 })}
         </ul>
     );
