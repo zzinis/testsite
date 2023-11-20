824d2e21273fe116ab3cca6afd9c987af0db82ee
zzinis <lizzyin@naver.com>
2023-11-20 19:27:59 +0900

    첫번째 버튼을 일단 활성화 처리

--- site/src/components/main/Btns.js
+++ site/src/components/main/Btns.js
@@ -2,6 +2,8 @@ import { useRef, useEffect, useState } from 'react';
 import Anime from '../../asset/anime';
 
 function Btns() {
+    console.log('btn Called');
+
     const btnRef = useRef(null);
     const pos = useRef([]);
     const [Num, setNum] = useState(0);
@@ -25,9 +27,12 @@ function Btns() {
             {Array(Num)
                 .fill()
                 .map((_, idx) => {
+                    let defaultClass = '';
+                    if (idx === 0) defaultClass = 'on';
                     return (
                         <li
                             key={idx}
+                            className={defaultClass}
                             onClick={() => {
                                 new Anime(window, {
                                     prop: 'scroll',
