348141dddbecbe8d56a0817f39b004c5ac4ec045
zzinis <lizzyin@naver.com>
2023-11-21 21:57:38 +0900

    news article 추가

--- site/src/components/main/News.js
+++ site/src/components/main/News.js
@@ -3,7 +3,10 @@ import React from 'react'
 function News() {
     return (
         <section id='news' className='myScroll'>
-            News
+            <article></article>
+            <article></article>
+            <article></article>
+            <article></article>
         </section>
     );
 }
