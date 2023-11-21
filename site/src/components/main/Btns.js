6f9489e9a4faa18d46179f75407c2ace007d7923
zzinis <lizzyin@naver.com>
2023-11-21 21:56:48 +0900

    box활성화 및 활성화 기준점 변경

--- site/src/components/main/Btns.js
+++ site/src/components/main/Btns.js
@@ -15,12 +15,16 @@ function Btns() {
     };
 
     const activation = () => {
+        const base = -window.innerHeight / 2;
         const scroll = window.scrollY;
         const btns = btnRef.current.children;
+        const boxs = btnRef.current.parentElement.querySelectorAll('.myScroll');
         pos.current.forEach((pos, idx) => {
-            if (scroll >= pos) {
+            if (scroll >= pos + base) {
                 for (const btn of btns) btn.classList.remove('on');
+                for (const box of boxs) box.classList.remove('on');
                 btns[idx].classList.add('on');
+                boxs[idx].classList.add('on');
             }
         });
     };
