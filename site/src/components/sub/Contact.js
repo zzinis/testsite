8191af850252e48667a3a956e9312edc87f024c8
zzinis <lizzyin@naver.com>
2023-11-23 21:11:45 +0900

    리액트 버전에 맞게 기본 지도 인스턴스 생성

--- site/src/components/sub/Contact.js
+++ site/src/components/sub/Contact.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import Layout from '../common/Layout';
+import { useRef, useEffect } from 'react';
 
 function Contact() {
-    return <Layout name={'Contact'}></Layout>;
+    //지도가 들어갈 프레임도 가상요소 참조를 위해 useRef로 참조 객체 생성
+    const container = useRef(null);
+    //일반 HTML버전과는 달리 윈도우객체에서 직접 Kakao 상위 객체값을 뽑아옴
+    const { kakao } = window;
+    const option = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3, // 지도의 확대 레벨
+    };
+    useEffect(() => {
+        //인스턴스 호출구문은 컴포넌트 처음 마운트시 호출
+        new kakao.maps.Map(container.current, option);
+    }, []);
+    return (
+        <Layout name={'Contact'}>
+            <div id='map' ref={container}></div>
+        </Layout>
+    );
 }
 
 export default Contact
