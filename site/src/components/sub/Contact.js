08e87ad3b7aecf4bf703ee48b31fdc633fe1e2e5
zzinis <lizzyin@naver.com>
2023-11-24 21:05:54 +0900

    마커 이미지 변경

--- site/src/components/sub/Contact.js
+++ site/src/components/sub/Contact.js
@@ -3,20 +3,22 @@ import Layout from '../common/Layout';
 import { useRef, useEffect } from 'react';
 
 function Contact() {
-    //지도가 들어갈 프레임도 가상요소 참조를 위해 useRef로 참조 객체 생성
+
     const container = useRef(null);
-    //일반 HTML버전과는 달리 윈도우객체에서 직접 Kakao 상위 객체값을 뽑아옴
     const { kakao } = window;
     const option = {
-        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-        level: 3, // 지도의 확대 레벨
+        center: new kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3,
     };
     useEffect(() => {
-        //인스턴스 호출구문은 컴포넌트 처음 마운트시 호출
-        new kakao.maps.Map(container.current, option);
         const mapInstance = new kakao.maps.Map(container.current, option);
+        const imgSrc = `${process.env.PUBLIC_URL}/img/marker1.png`;
+        const imgSize = new kakao.maps.Size(232, 99);
+        const imgPos = { offset: new kakao.maps.Point(116, 99) };
+        const markerImage = new kakao.maps.MarkerImage(imgSrc, imgSize, imgPos);
         const marker = new kakao.maps.Marker({
             position: option.center,
+            image: markerImage,
         });
         marker.setMap(mapInstance);
     }, []);
