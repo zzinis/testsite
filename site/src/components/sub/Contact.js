e0e8422a2781e26a266c18cdd683255aebc6e297
zzinis <lizzyin@naver.com>
2023-11-26 21:43:53 +0900

    동적으로 지점 버튼 생성후 버튼클릭시마다 지도 다시 랜더링

--- site/src/components/sub/Contact.js
+++ site/src/components/sub/Contact.js
@@ -5,7 +5,8 @@ function Contact() {
     const [Traffic, setTraffic] = useState(false);
     const [Location, setLocation] = useState(null);
     const container = useRef(null);
-
+    const [Index, setIndex] = useState(0);
+    console.log(Index);
 
     //아래 정보값들은 useEffect구문에서 인스턴스 생성할때만 필요한 정보값에 불과하므로 미리 읽히도록 useEffect바깥에 배치
     const { kakao } = window;
@@ -32,26 +33,37 @@ function Contact() {
             imgPos: { offset: new kakao.maps.Point(116, 99) },
         },
     ];
-    const option = { center: info[0].latlng, level: 3 };
-    const imgSrc = info[0].imgSrc;
-    const imgSize = info[0].imgSize;
-    const imgPos = info[0].imgPos;
+    const option = { center: info[Index].latlng, level: 3 };
+    const imgSrc = info[Index].imgSrc;
+    const imgSize = info[Index].imgSize;
+    const imgPos = info[Index].imgPos;
     const markerImage = new kakao.maps.MarkerImage(imgSrc, imgSize, imgPos);
     const marker = new kakao.maps.Marker({ position: option.center, image: markerImage });
     useEffect(() => {
         const mapInstance = new kakao.maps.Map(container.current, option);
 
         setLocation(mapInstance);
-    }, []);
+    }, [Index]);
     useEffect(() => {
-
-        Traffic ? Location?.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC) : Location?.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+        Traffic
+            ? Location?.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
+            : Location?.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
     }, [Traffic]);
 
     return (
         <Layout name={'Contact'}>
             <div id='map' ref={container}></div>
             <button onClick={() => setTraffic(!Traffic)}>{Traffic ? 'Traffic ON' : 'Traffic OFF'}</button>
+            {/* 배열정보값을 토대로 동적으로 li지점버튼 생성하고 해당 버튼 클릭할때 순서값 State를 변경하면서 지도화면이 갱신되도록 수정 */}
+            <ul className='branch'>
+                {info.map((el, idx) => {
+                    return (
+                        <li key={idx} onClick={() => setIndex(idx)}>
+                            {el.title}
+                        </li>
+                    );
+                })}
+            </ul>
         </Layout>
     );
 }
