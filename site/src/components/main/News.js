b17495fa3978e6e2ce34b8f3dd8e451c50924157
zzinis <lizzyin@naver.com>
2023-12-16 22:16:32 +0900

    메인 페이지 컨텐츠 미리보기 출력

--- site/src/components/main/News.js
+++ site/src/components/main/News.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import { useState } from 'react';
 
 function News() {
+    const getLocalData = () => {
+        const data = localStorage.getItem('post');
+        return JSON.parse(data);
+    };
+    const [Posts] = useState(getLocalData());
     return (
         <section id='news' className='myScroll'>
-            <article></article>
-            <article></article>
-            <article></article>
-            <article></article>
+            {Posts.map((post, idx) => {
+                if (idx >= 4) return null;
+                return (
+                    <article key={idx}>
+                        <h3>{post.title}</h3>
+                        <p>{post.content}</p>
+                    </article>
+                );
+            })}
         </section>
     );
 }
