cf8983119080e275fb4ef2fb39228c3387aa7da3
zzinis <lizzyin@naver.com>
2023-12-18 21:32:33 +0900

    gkatndp opt추가

--- site/src/components/sub/Gallery.js
+++ site/src/components/sub/Gallery.js
@@ -7,16 +7,21 @@ function Gallery() {
         const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
         const key = '';
         const method_interest = 'flickr.interestingness.getList';
+        const method_user = 'flickr.people.getPhotos';
+        const method_search = 'flickr.photos.search';
         const num = 20;
         let url = '';
         if (opt.type === 'interest') url = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
+        if (opt.type === 'search')
+            url = `${baseURL}&api_key=${key}&method=${method_search}&per_page=${num}&tags=${opt.tags}`;
+        if (opt.type === 'user')
+            url = `${baseURL}&api_key=${key}&method=${method_user}&per_page=${num}&user_id=${opt.user}`;
 
         const result = await axios.get(url);
-        console.log(result.data.photos.photo);
         setItems(result.data.photos.photo);
     };
 
-    useEffect(() => getFlickr({ type: 'interest' }), []);
+    useEffect(() => getFlickr({ type: 'user', user: '' }), []);
 
     return (
         <Layout name={'Gallery'}>
