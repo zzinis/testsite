4031c1af900581cdb2d06a5956b71a65fda5fc8f
zzinis <lizzyin@naver.com>
2023-12-20 20:01:42 +0900

    자식 요소에 줄바꿈 되는 텍스트를 전달

--- site/src/components/common/Layout.js
+++ site/src/components/common/Layout.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react';
 
-function Layout({ name, children }) {
+function Layout({ name, children, txt = 'Default' }) {
     const frame = useRef(null);
     useEffect(() => {
         frame.current.classList.add('on');
@@ -10,6 +10,17 @@ function Layout({ name, children }) {
             <figure></figure>
             <div className='inner'>
                 <h1>{name}</h1>
+                <h2>
+                    {/* 해당 구분자로 문자값을 배열로 분리하고 반복을 돌면서 b태그 추가: 이때 주의할점 : 반복도는 요소이므로 key속성을 적용하기 위해서는 React.Fragement형태로 wrapping처리 */}
+                    {txt.split('-').map((el, idx) => {
+                        return (
+                            <React.Fragment key={idx}>
+                                {el}
+                                <br />
+                            </React.Fragment>
+                        );
+                    })}
+                </h2>
                 {children}
             </div>
         </section>
