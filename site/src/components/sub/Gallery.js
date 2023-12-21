242f1cc15be97acd3a05978217249707a9891d37
zzinis <lizzyin@naver.com>
2023-12-21 21:12:15 +0900

    카운터값 초기화

--- site/src/components/sub/Gallery.js
+++ site/src/components/sub/Gallery.js
@@ -11,6 +11,9 @@ function Gallery() {
     const [Loader, setLoader] = useState(true);
 
     const getFlickr = async (opt) => {
+        //새롭게 data fetching이 실행되면 참조객체에 담겨있는 카운터 값을 다시 0으로 초기화
+        //useRef로 참조한 값은 컴포넌트가 재실행되더라도 일반 변수처럼 초기화되는 것이 아니라 직접 초기화해야됨
+        counter.current = 0;
         const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
         const key = '';
         const method_interest = 'flickr.interestingness.getList';
@@ -34,6 +37,8 @@ function Gallery() {
             img.onload = () => {
                 //내부적으로 카운터값을 1씩 증가
                 ++counter.current;
+                console.log(counter);
+
 
                 //로딩완료된 이미지수와 전체이미지수가 같아지면
                 if (counter.current === imgs.length) {
@@ -49,6 +54,24 @@ function Gallery() {
 
     return (
         <Layout name={'Gallery'}>
+            <button
+                onClick={() => {
+                    setLoader(true);
+                    frame.current.classList.remove('on');
+                    getFlickr({ type: 'interest' });
+                }}
+            >
+                Interest Gallery
+            </button>
+            <button
+                onClick={() => {
+                    setLoader(true);
+                    frame.current.classList.remove('on');
+                    getFlickr({ type: 'user', user: '164021883@N04' });
+                }}
+            >
+                My Gallery
+            </button>
             <div className='frame' ref={frame}>
                 <Masonry elementType={'div'} options={{ transitionDuration: '0.5s' }}>
                     {Items.map((item, idx) => {
