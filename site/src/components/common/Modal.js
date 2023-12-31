224361c475ad878c3f1ddad56e9cd83a65c69dcd
zzinis <lizzyin@naver.com>
2023-12-31 23:24:44 +0900

    모달안쪽에 iframe같이 무거운 요소를 바로 모션처리시 지글거리는 현상 해결

--- site/src/components/common/Modal.js
+++ site/src/components/common/Modal.js
@@ -26,8 +26,14 @@ const Modal = forwardRef((props, ref) => {
                     exit={{ opacity: 0, scale: 0, transition: { duration: 0.5, delay: 0.5 } }}
 
                 >
-                    <div className='con'>{props.children}</div>
-                    <motion.span
+                    <motion.div
+                        className='con'
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1, transition: { delay: 0.5 } }}
+                        exit={{ opacity: 0, transition: { delay: 0 } }}
+                    >
+                        {props.children}
+                    </motion.div>                    <motion.span
                         className='close'
                         onClick={() => setOpen(false)}
                         initial={{ opacity: 0, y: 100 }}
