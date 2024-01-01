b039d547100f28a1b1ad95161aea19fa415a76b9
zzinis <lizzyin@naver.com>
2024-01-01 21:01:31 +0900

    Menu컴포넌트 생성 및 forwardRef처리

--- site/src/components/common/Header.js
+++ site/src/components/common/Header.js
@@ -2,50 +2,55 @@ import React from 'react'
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link, NavLink } from 'react-router-dom';
+import Menu from './Menu';
+
 
 function Header({ type }) {
     const active = 'on';
 
     return (
-        <header className={type}>
-            <h1>
-                <Link to='/'>LOGO</Link>
-            </h1>
+        <>
+            <header className={type}>
+                <h1>
+                    <Link to='/'>LOGO</Link>
+                </h1>
+                <ul id='gnb'>
+                    <li>
+                        <NavLink to='/department' activeClassName={active}>
+                            Department
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/community' activeClassName={active}>
+                            Community
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/gallery' activeClassName={active}>
+                            Gallery
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/youtube' activeClassName={active}>
+                            Youtube
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/contact' activeClassName={active}>
+                            Contact
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/member' activeClassName={active}>
+                            Member
+                        </NavLink>
+                    </li>
+                </ul>
+                <FontAwesomeIcon icon={faBars} />
+            </header>
 
-            <ul id='gnb'>
-                <li>
-                    <NavLink to='/department' activeClassName={active}>
-                        Department
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/community' activeClassName={active}>
-                        Community
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/gallery' activeClassName={active}>
-                        Gallery
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/youtube' activeClassName={active}>
-                        Youtube
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/contact' activeClassName={active}>
-                        Contact
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/member' activeClassName={active}>
-                        Member
-                    </NavLink>
-                </li>
-            </ul>
-            <FontAwesomeIcon icon={faBars} />
-        </header>
+            <Menu />
+        </>
     );
 }
 
