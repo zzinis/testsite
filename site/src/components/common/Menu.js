deb9dc4fa3ebb7fc27c3fe9daa5bec8f52276702
zzinis <lizzyin@naver.com>
2024-01-03 21:43:26 +0900

    메뉴 모션 처리

--- site/src/components/common/Menu.js
+++ site/src/components/common/Menu.js
@@ -1,4 +1,6 @@
 import { forwardRef, useImperativeHandle, useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+
 
 const Menu = forwardRef((props, ref) => {
     const [Open, setOpen] = useState(false);
@@ -7,13 +9,18 @@ const Menu = forwardRef((props, ref) => {
         return { toggle: () => setOpen(!Open) };
     });
     return (
-        <>
+        <AnimatePresence>
             {Open && (
-                <nav id='mobilePanel'>
+                <motion.nav
+                    id='mobilePanel'
+                    initial={{ opacity: 0, x: -280 }}
+                    animate={{ opacity: 1, x: 0, transition: { duration: 0.5 } }}
+                    exit={{ opacity: 0, x: -280, transition: { duration: 0.5 } }}
+                >
                     <h1>LOGO</h1>
-                </nav>
+                </motion.nav>
             )}
-        </>
+        </AnimatePresence>
     );
 });
 
