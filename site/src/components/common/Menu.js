f25ed06a47b84dfb6bb4a9d41d3906c183ef5a7b
zzinis <lizzyin@naver.com>
2024-01-03 21:44:07 +0900

    모바일 메뉴 이동

--- site/src/components/common/Menu.js
+++ site/src/components/common/Menu.js
@@ -1,8 +1,11 @@
 import { forwardRef, useImperativeHandle, useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
+import { NavLink, Link } from 'react-router-dom';
 
 
 const Menu = forwardRef((props, ref) => {
+    const active = { color: 'aqua' };
+
     const [Open, setOpen] = useState(false);
 
     useImperativeHandle(ref, () => {
@@ -17,8 +20,42 @@ const Menu = forwardRef((props, ref) => {
                     animate={{ opacity: 1, x: 0, transition: { duration: 0.5 } }}
                     exit={{ opacity: 0, x: -280, transition: { duration: 0.5 } }}
                 >
-                    <h1>LOGO</h1>
-                </motion.nav>
+                    <h1>
+                        <Link to='/'>LOGO</Link>
+                    </h1>
+
+                    <ul id='gnbMo'>
+                        <li>
+                            <NavLink to='/department' activeStyle={active}>
+                                Department
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/community' activeStyle={active}>
+                                Community
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/gallery' activeStyle={active}>
+                                Gallery
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/youtube' activeStyle={active}>
+                                Youtube
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/contact' activeStyle={active}>
+                                Contact
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/member' activeStyle={active}>
+                                Members
+                            </NavLink>
+                        </li>
+                    </ul>                </motion.nav>
             )}
         </AnimatePresence>
     );
