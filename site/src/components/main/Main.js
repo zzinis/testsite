aa1face19ecc1f0e58ea68d2161113162d0a7750
zzinis <lizzyin@naver.com>
2024-01-05 22:24:19 +0900

    prop drilling의 개념과 이를 해결하기 위한 redux의 개념 소개

--- site/src/components/main/Main.js
+++ site/src/components/main/Main.js
@@ -7,6 +7,15 @@ import Vids from './Vids';
 import Banner from './Banner';
 
 import Btns from './Btns';
+
+
+/*
+    Prop drilling
+    - 특정 값을 자식 컴포넌트에게 전달하기 위해서 불필요하게 많은 중간 컴포넌트들이 값을 전달목적으로 쓰이는 경우
+
+    Redux
+    위와 같은 prop drilling을 방지하기 위해서 복잡하게 컴포넌트를 통해 전달할 값을 prop아닌 컴포넌트외부에 데이터 전용 객체를 만들어서 어떤 위치의 컴포넌트에서든 편하게 값을 가져오고 수정할 수 있게 만든 전역 데이터 체계
+*/
 function Main({ menu }) {
     const [Scrolled, setScrolled] = useState(0);
     const [Pos, setPos] = useState([]);
