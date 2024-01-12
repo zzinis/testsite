ef7390b4e80de9da6e17a7bf6e9206f68caefe0f
zzinis <lizzyin@naver.com>
2024-01-12 21:09:36 +0900

    원하는 컴포넌트에서 전역 데이터 호출

--- site/src/components/main/Vids.js
+++ site/src/components/main/Vids.js
@@ -1,6 +1,9 @@
 import { memo } from 'react';
+import { useSelector } from 'react-redux';
 
 function Vids() {
+    useSelector((store) => console.log(store));
+
     return (
         <section id='vids' className='myScroll'>
             Vids
