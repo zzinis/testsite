acb492b0fbfa724034bb6f028c2236b5cb101fe9
zzinis <lizzyin@naver.com>
2024-01-15 22:25:13 +0900

    기본 스와이퍼 기능 연결

--- site/src/components/main/Vids.js
+++ site/src/components/main/Vids.js
@@ -1,22 +1,28 @@
 import { memo } from 'react';
 import { useSelector } from 'react-redux';
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 function Vids() {
     const Vids = useSelector((store) => store.youtube.data);
     console.log(Vids);
     return (
         <section id='vids' className='myScroll'>
-            {Vids.map((vid, idx) => {
-                if (idx >= 5) return null;
+            <Swiper>
+                {Vids.map((vid, idx) => {
+                    if (idx >= 5) return null;
 
-                return (
-                    <article key={idx}>
-                        <div className='pic'>
-                            <img src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
-                        </div>
-                    </article>
-                );
-            })}        </section>
+                    return (
+                        <SwiperSlide key={idx}>
+                            <div className='inner'>
+                                <div className='pic'>
+                                    <img src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    );
+                })}
+            </Swiper>
+        </section>
     );
 }
 
