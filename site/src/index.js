3c9bd8bbf5bb47a3bc91dccecd0b69ee74bf0f0e
zzinis <lizzyin@naver.com>
2024-01-19 17:58:04 +0900

    flikr reducer

--- site/src/index.js
+++ site/src/index.js
@@ -6,12 +6,16 @@ import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import youtubeReducer from './redux/youtubeSlice';
 import departmentReducer from './redux/departmentSlice';
+import flickrReducer from './redux/flickrSlice';
+
 
 
 const store = configureStore({
   reducer: {
     youtube: youtubeReducer,
     department: departmentReducer,
+    flickr: flickrReducer,
+
 
   },
 });
