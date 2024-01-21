b9e901c815cbaf25b111db12364e3a3cc606a698
zzinis <lizzyin@naver.com>
2024-01-21 15:47:02 +0900

    flickrReducr데이터 호출

--- site/src/components/main/Pics.js
+++ site/src/components/main/Pics.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 function Pics({ Scrolled, Pos }) {
+    useSelector((store) => console.log(store));
+
     const currentPos = Scrolled - Pos;
     const base = window.innerHeight / 2;
     const modified = currentPos + base;
@@ -9,8 +12,8 @@ function Pics({ Scrolled, Pos }) {
             <h1 style={{ transform: `translateX(${currentPos}px)` }}>FLICKR</h1>
             <article
                 style={{
-                    transform: `translate(-50%, -50%) rotate(${Scrolled >= Pos - base ? modified : 0}deg) scale(${Scrolled >= Pos - base ? 1 + modified / 500 : 1}) `,
-                    opacity: `${Scrolled >= Pos - base ? 1 - modified / 500 : 1}`,
+                    transform: `translate(-50%, -50%) rotate(${Scrolled >= Pos - base ? modified : 0}deg) scale(${Scrolled >= Pos - base ? 1 + modified / 500 : 1
+                        }) `,
                 }}
             ></article>
         </section>
