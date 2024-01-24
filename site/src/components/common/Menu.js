b601d09a3c8b281d04009880b08c64dd59e18529
zzinis <lizzyin@naver.com>
2024-01-24 20:43:08 +0900

    전역 state값과 해당 state값을 변경할수 잇는 함수를 menu slice로 import후 이벤트 연결

--- site/src/components/common/Menu.js
+++ site/src/components/common/Menu.js
@@ -1,32 +1,35 @@
-import { forwardRef, useImperativeHandle, useState, useEffect } from 'react';
+/*
+    --Redux-tookit으로 client State전역 관리하는 작업 순서--
+    {open: false} false면 메뉴제거 / true면 메뉴오픈 
+    menuSlice.js를 만들어서 위의 정보값을 초기 전역 state로 등록
+    reducer에는 해당 전역 state값을 변경해주는 함수를 등록 (close, toggle)
+    해당 함수를 원하는 컴포넌트에서 자유롭게 호출해서 전역 state변경하도록 
+*/
+
+import { useSelector, useDispatch } from 'react-redux';
+//menuSlice로 부터 전역state값을 변경해주는 close함수를 import
+import { close } from '../../redux/menuSlice';
 import { motion, AnimatePresence } from 'framer-motion';
 import { NavLink, Link } from 'react-router-dom';
 
 
-const Menu = forwardRef((props, ref) => {
+function Menu() {
     const active = { color: 'aqua' };
 
-    const [Open, setOpen] = useState(false);
+    const dispatch = useDispatch();
+    const menu = useSelector((store) => store.menu.open);
 
-    useEffect(() => {
-        window.addEventListener('resize', () => {
-            if (window.innerWidth >= 1200) setOpen(false);
-        });
-    }, []);
 
-    useImperativeHandle(ref, () => {
-        return { toggle: () => setOpen(!Open) };
-    });
     return (
         <AnimatePresence>
-            {Open && (
+            {menu && (
                 <motion.nav
                     id='mobilePanel'
                     initial={{ opacity: 0, x: -280 }}
                     animate={{ opacity: 1, x: 0, transition: { duration: 0.5 } }}
                     exit={{ opacity: 0, x: -280, transition: { duration: 0.5 } }}
-                    onClick={() => setOpen(false)}
-
+                    //닫기버튼 클릭시 전역state를 변경하는 close함수를 호출해서 그 결과값인 action객체를 dispatch로 전달
+                    onClick={() => dispatch(close())}
                 >
                     <h1>
                         <Link to='/'>LOGO</Link>
@@ -67,6 +70,6 @@ const Menu = forwardRef((props, ref) => {
             )}
         </AnimatePresence>
     );
-});
+}
 
 export default Menu;
