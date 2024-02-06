ef1a116ab6685ec21ac2db7792192c056f2cd4d2
zzinis <lizzyin@naver.com>
2024-02-06 21:32:47 +0900

    flickr데이터 react-query처리

--- site/src/index.js
+++ site/src/index.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 // import youtubeReducer from './redux/youtubeSlice';
 import departmentReducer from './redux/departmentSlice';
-import flickrReducer from './redux/flickrSlice';
+// import flickrReducer from './redux/flickrSlice';
 import menuReducer from './redux/menuSlice';
 
 
@@ -15,7 +15,7 @@ const store = configureStore({
   reducer: {
     // youtube: youtubeReducer,
     department: departmentReducer,
-    flickr: flickrReducer,
+    // flickr: flickrReducer,
     menu: menuReducer,
 
 
