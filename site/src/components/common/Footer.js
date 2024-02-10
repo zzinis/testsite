f2ae0377ab393a2b34ae6ad167e4b877d7078d85
zzinis <lizzyin@naver.com>
2024-02-10 22:40:19 +0900

    department custom react-query적용1

--- site/src/components/common/Footer.js
+++ site/src/components/common/Footer.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useDepartmentQuery } from '../../hooks/useDepartmentQuery';
 
 
 function Footer() {
-    const Department = useSelector((store) => store.department.data);
+    const { data: Department, isSuccess } = useDepartmentQuery();
 
     return (
         <footer>
             <h1>DCODELAB</h1>
             <p>2023 DCODELAB &copy; ALL RIGHTS RESERVED.</p>
-            <p>{`This Company was founded by ${Department[0]?.name} in 2023`}</p>
+            <p>{`This Company was founded by ${isSuccess && Department[0].name} in 2023`}</p>
 
         </footer>
     );
