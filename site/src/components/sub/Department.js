4a74de1d969c390741c933137759c44e25a8e0e1
zzinis <lizzyin@naver.com>
2024-02-11 21:08:20 +0900

    department custom react-query적용2

--- site/src/components/sub/Department.js
+++ site/src/components/sub/Department.js
@@ -1,28 +1,29 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useDepartmentQuery } from '../../hooks/useDepartmentQuery';
 
 
 import Layout from '../common/Layout';
 
 function Department() {
-    const Members = useSelector((store) => store.department.data);
+    const { data: Members, isSuccess } = useDepartmentQuery();
 
     return (
         // props으로 자식요소에 줄바꿈될 텍스트를 전달하고 싶을떄
         //줄바꿀될 부분에 구분자가 될 문자값을 넣어서 전달
         <Layout name={'Department'} txt={'Hello-World'} bg={'Department.jpg'}>
-            {Members.map((member, idx) => {
-                return (
-                    <article key={idx}>
-                        <div className='pic'>
-                            <img src={`${process.env.PUBLIC_URL}/img/${member.pic}`} alt={member.name} />
-                            <img src={`${process.env.PUBLIC_URL}/img/${member.pic}`} alt={member.name} />
-                        </div>
-                        <h2>{member.name}</h2>
-                        <p>{member.position}</p>
-                    </article>
-                );
-            })}
+            {isSuccess &&
+                Members.map((member, idx) => {
+                    return (
+                        <article key={idx}>
+                            <div className='pic'>
+                                <img src={`${process.env.PUBLIC_URL}/img/${member.pic}`} alt={member.name} />
+                                <img src={`${process.env.PUBLIC_URL}/img/${member.pic}`} alt={member.name} />
+                            </div>
+                            <h2>{member.name}</h2>
+                            <p>{member.position}</p>
+                        </article>
+                    );
+                })}
         </Layout>
     );
 }
