62fda0f5ce8c56ac5ecd0c500240b46cd29b2d84
zzinis <lizzyin@naver.com>
2024-02-11 21:09:30 +0900

    department custom react-query적용4

--- site/src/index.js
+++ site/src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 // import youtubeReducer from './redux/youtubeSlice';
-import departmentReducer from './redux/departmentSlice';
 // import flickrReducer from './redux/flickrSlice';
 import menuReducer from './redux/menuSlice';
 
@@ -14,7 +13,7 @@ import menuReducer from './redux/menuSlice';
 const store = configureStore({
   reducer: {
     // youtube: youtubeReducer,
-    department: departmentReducer,
+    // department: departmentReducer,
     // flickr: flickrReducer,
     menu: menuReducer,
 
