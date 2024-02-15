a937a5d9f14b6b048481bd0c82c1f9da226faa22
zzinis <lizzyin@naver.com>
2024-02-15 19:31:18 +0900

    useGlobalData2

--- site/src/components/common/Menu.js
+++ site/src/components/common/Menu.js
@@ -1,41 +1,35 @@
-/*
-    --Redux-tookit으로 client State전역 관리하는 작업 순서--
-    {open: false} false면 메뉴제거 / true면 메뉴오픈 
-    menuSlice.js를 만들어서 위의 정보값을 초기 전역 state로 등록
-    reducer에는 해당 전역 state값을 변경해주는 함수를 등록 (close, toggle)
-    해당 함수를 원하는 컴포넌트에서 자유롭게 호출해서 전역 state변경하도록 
-*/
+// /*
+//     --Redux-tookit으로 client State전역 관리하는 작업 순서--
+//     {open: false} false면 메뉴제거 / true면 메뉴오픈 
+//     menuSlice.js를 만들어서 위의 정보값을 초기 전역 state로 등록
+//     reducer에는 해당 전역 state값을 변경해주는 함수를 등록 (close, toggle)
+//     해당 함수를 원하는 컴포넌트에서 자유롭게 호출해서 전역 state변경하도록 
+// */
 
-import { useSelector, useDispatch } from 'react-redux';
-//menuSlice로 부터 전역state값을 변경해주는 close함수를 import
-import { close } from '../../redux/menuSlice';
+// import { useSelector, useDispatch } from 'react-redux';
+// //menuSlice로 부터 전역state값을 변경해주는 close함수를 import
+// import { close } from '../../redux/menuSlice';
 import { motion, AnimatePresence } from 'framer-motion';
 import { NavLink, Link } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useGlobalData } from '../../hooks/useGlobalContext';
 
 
 function Menu() {
     const active = { color: 'aqua' };
 
-    const dispatch = useDispatch();
-    const menu = useSelector((store) => store.menu.open);
+    const { MenuOpen, setMenuOpen } = useGlobalData();
 
-    useEffect(() => {
-        window.addEventListener('resize', () => {
-            if (window.innerWidth >= 1200) dispatch(close());
-        });
-    }, [dispatch]);
 
     return (
         <AnimatePresence>
-            {menu && (
+            {MenuOpen && (
                 <motion.nav
                     id='mobilePanel'
                     initial={{ opacity: 0, x: -280 }}
                     animate={{ opacity: 1, x: 0, transition: { duration: 0.5 } }}
                     exit={{ opacity: 0, x: -280, transition: { duration: 0.5 } }}
-                    //닫기버튼 클릭시 전역state를 변경하는 close함수를 호출해서 그 결과값인 action객체를 dispatch로 전달
-                    onClick={() => dispatch(close())}
+                    onClick={() => setMenuOpen(false)}
+
                 >
                     <h1>
                         <Link to='/'>LOGO</Link>
